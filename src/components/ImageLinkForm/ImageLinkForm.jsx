0eaf41bfac024960ffdd991211833af7850687e8
George Kefas <kefasgeo04@gmail.com>
2024-03-18 22:41:19 +0200

    changed UI

--- src/components/ImageLinkForm/ImageLinkForm.jsx
+++ src/components/ImageLinkForm/ImageLinkForm.jsx
@@ -1,19 +1,30 @@
 import './ImageLinkForm.css';
+import { MDBBtn } from 'mdb-react-ui-kit';
 
-const ImageLinkForm = ({onInputChange,onButtonSubmit}) => {
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
     return (
         <>
             <p className="f3 tc black">
                 {'Step into enchantment while this reveals hidden faces in your images. Try it now!'}
             </p>
-            <div className='center'>
-                <div className='w-90 center br3 shadow-5 formContainer'>
-                    <input className="f4 pa2 w-70" type="text" onChange={onInputChange} placeholder='Enter image url (png/jpg)'/>
+            {
+                /*
+                    
                     <button className="w-30 grow f4 link pa2 dib light-gray bg-light-purple"
-                    onClick={onButtonSubmit}>
+                    >
                     Detect</button>
                 </div>
+            </div>*/
+            }
+
+            <div className='center w-90 centerInputs'>
+                <input className="f4 pa2 w-70 inputUrl" type="text" onChange={onInputChange} placeholder='Enter image url' />
+                <MDBBtn className='w-30 detectBtn' onClick={onButtonSubmit}>
+                    Detect
+                </MDBBtn>
             </div>
+
+
         </>
     );
 }
